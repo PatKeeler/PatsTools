d8b48684b47ede3e196e4698526cdcaf77feaaf9
PatKeeler <keelerpl@gmail.com>
2021-11-24 15:00:32 -0700

    removed logging statements

--- frontend/src/app/format.service.ts
+++ frontend/src/app/format.service.ts
@@ -10,21 +10,22 @@ import { Observable } from 'rxjs';
 export class FormatService {
 
   apiUrl = 'http://localhost/PatsTools/api';
-  headers = new HttpHeaders({'Content-Type': 'application/json'});
+
+  headers = new HttpHeaders()
+    .set('Content-Type', 'application/json');
+
 
   constructor(private http: HttpClient) { }
 
   getSql(parameters: HttpParams): Observable<any> {
     const API_URL = `${this.apiUrl}/formatSql`;
-    console.log('SQL_URL: ' + API_URL);
-    console.log('SQL Params:            ' + parameters);
-    return this.http.post(API_URL, {params: parameters});
+    // console.log('SQL_URL: ' + API_URL);
+    return this.http.post(API_URL, parameters);
   }
 
   getXml(parameters: HttpParams): Observable<any> {
     const API_URL = `${this.apiUrl}/formatXml`;
-    console.log('XML_URL: ' + API_URL);
-    console.log('XML Params:            ' + parameters);
-    return this.http.post(API_URL, {params: parameters});
+    // console.log('XML_URL: ' + API_URL);
+    return this.http.post(API_URL, parameters);
   }
 }
